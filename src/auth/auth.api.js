4afe91e9e5062cf163464fc8cc8f7bb238d3b3bd
azer <azerprojects@gmail.com>
2021-05-25 10:47:40 +0700

    fix: resolve merge conflict

---
+++ src/auth/auth.api.js
@@ -0,0 +1,18 @@
+import { api } from '@/utils/app'
+import qs from 'qs'
+import { create } from '@/utils/errors'
+
+export async function login({ username = '', password = '' }) {
+  const response = await fetch(api.login, {
+    method: 'POST',
+    body: qs.stringify({ username, password }),
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+  })
+  console.log(response.status)
+  if (response.status >= 400)
+    throw create('auth', 'unknown', JSON.stringify({ username, password }))
+  return response.json()
+}
