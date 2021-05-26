9ca78aabb19317e6a96b6e6b25ed0ded523bfac5
azer <azerprojects@gmail.com>
2021-05-26 19:31:51 +0700

    feat: use webpack5, remove StyleProvider

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { StylesProvider, ThemeProvider } from '@material-ui/core'
-import { generateClassName, light } from '@/app.theme'
+import { ThemeProvider } from '@material-ui/core'
+import { light } from '@/app.theme'
 import dynamic from 'next/dynamic'
 import SnackBarProvider from '@/components/snackbar'
 import '@/app.css'
@@ -9,14 +9,12 @@ import '@/app.css'
 const DynamicPageLoading = dynamic(() => import('@/components/page-loading'))
 export default function MainApp({ Component, pageProps }) {
   return (
-    <StylesProvider generateClassName={generateClassName}>
-      <ThemeProvider theme={light}>
-        <DynamicPageLoading />
-        <SnackBarProvider>
-          <Component {...pageProps} />
-        </SnackBarProvider>
-      </ThemeProvider>
-    </StylesProvider>
+    <ThemeProvider theme={light}>
+      <DynamicPageLoading />
+      <SnackBarProvider>
+        <Component {...pageProps} />
+      </SnackBarProvider>
+    </ThemeProvider>
   )
 }
 
