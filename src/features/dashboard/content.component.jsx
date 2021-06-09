12f4ac89da5f05f8c1dd32474c6c967b04e0336e
azer <azerprojects@gmail.com>
2021-06-09 17:42:00 +0700

    dev(dashboard): filter and sorting

--- src/features/dashboard/content.component.jsx
+++ src/features/dashboard/content.component.jsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
 import {
   Box,
   Collapse,
   FormControl,
   FormControlLabel,
-  FormLabel,
   Grid,
   IconButton,
   Radio,
@@ -12,13 +12,46 @@ import {
   Tooltip,
   Typography
 } from '@material-ui/core'
-import { Sort, FilterList } from '@material-ui/icons'
-import { ToggleButton } from '@material-ui/lab'
+import { FilterList } from '@material-ui/icons'
 import clsx from 'clsx'
-import useStyles from './content.style'
+import useStyles, { FilterButton } from './content.style'
 import { useDashboard } from './dashboard.context'
-import { currency } from '@/utils/intl'
 import { CourseSkeleton } from '@/components/course'
+
+const filters = [
+  {
+    title: 'difficulty',
+    options: [
+      { value: 1, label: 'beginner' },
+      { value: 2, label: 'advanced' },
+      { value: 3, label: 'expert' }
+    ]
+  },
+  {
+    title: 'rating',
+    options: [
+      { value: 4, label: '> 4 stars' },
+      { value: 3, label: '> 3 stars' },
+      { value: 2, label: '> 2 stars' },
+      { value: 1, label: '> 1 star' },
+      { value: 0, label: '< 1 star' }
+    ]
+  },
+  {
+    title: 'upload',
+    options: [
+      { value: 3600 * 24 * 7, label: 'last week' },
+      { value: 3600 * 24 * 30, label: 'last month' },
+      { value: 3600 * 24 * 30 * 6, label: 'last 6 months' },
+      { value: 3600 * 24 * 30 * 12, label: 'last year' }
+    ]
+  }
+]
+
+const sort = [
+  { value: 'view', label: 'views' },
+  { value: 'rating', label: 'ratings' }
+]
 
 export default function DashboardContent() {
   const { drawer } = useDashboard()
@@ -40,103 +73,66 @@ export default function DashboardContent() {
         </Tooltip>
       </Box>
       <Collapse in={filter}>
-        <Grid container spacing={1}>
+        <Grid container spacing={4} justify="center">
+          {filters.map((e) => (
+            <Grid item key={e.title}>
+              <FilterGroup title={e.title} options={e.options} />
+            </Grid>
+          ))}
           <Grid item>
-            <Box display="flex" flexDirection="column">
-              <ToggleButton>difficulty</ToggleButton>
-              <FormControl component="fieldset">
-                <RadioGroup aria-label="quiz" name="quiz">
-                  <FormControlLabel
-                    value="1-week"
-                    control={<Radio />}
-                    label="beginner"
-                  />
-                </RadioGroup>
-              </FormControl>
-            </Box>
-          </Grid>
-          <Grid item>
-            <ToggleButton>rating</ToggleButton>
-            <RadioGroup aria-label="quiz" name="quiz">
-              <FormControlLabel
-                value="1-week"
-                control={<Radio />}
-                label="above 5 stars"
-              />
-            </RadioGroup>
-          </Grid>
-          <Grid item>
-            <ToggleButton>price</ToggleButton>
-            <RadioGroup aria-label="quiz" name="quiz">
-              <FormControlLabel
-                value="1-week"
-                control={<Radio />}
-                label={`< ${currency(100)}`}
-              />
-            </RadioGroup>
-          </Grid>
-          <Grid item>
-            <ToggleButton>date</ToggleButton>
-            <RadioGroup aria-label="quiz" name="quiz">
-              <FormControlLabel
-                value="1-week"
-                control={<Radio />}
-                label="Last week"
-              />
-              <FormControlLabel
-                value="1-month"
-                control={<Radio />}
-                label="Last month"
-              />
-              <FormControlLabel
-                value="6-month"
-                control={<Radio />}
-                label="Last 6 months"
-              />
-              <FormControlLabel
-                value="1-year"
-                control={<Radio />}
-                label="Last year"
-              />
-            </RadioGroup>
-          </Grid>
-          <Grid item>
-            <Typography variant="button">SORT</Typography>
-            <RadioGroup aria-label="quiz" name="quiz">
-              <FormControlLabel
-                value="1-week"
-                control={<Radio />}
-                label="Views"
-              />
-              <FormControlLabel
-                value="1-week"
-                control={<Radio />}
-                label="Rating"
-              />
-            </RadioGroup>
+            <FilterGroup title="sort" options={sort} selected={0} />
           </Grid>
         </Grid>
       </Collapse>
-      <Grid container spacing={2}>
-        <Grid item>
-          <CourseSkeleton />
-        </Grid>
-        <Grid item>
-          <CourseSkeleton />
-        </Grid>
-        <Grid item>
-          <CourseSkeleton />
-        </Grid>
-        <Grid item>
-          <CourseSkeleton />
-        </Grid>
-        <Grid item>
-          <CourseSkeleton />
-        </Grid>
-        <Grid item>
-          <CourseSkeleton />
-        </Grid>
+      <Grid container spacing={2} component="ul">
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9].map((i) => (
+          <Grid item key={i}>
+            <CourseSkeleton />
+          </Grid>
+        ))}
       </Grid>
     </main>
   )
+}
+
+function FilterGroup({ toggled, onToggled, title, options, selected }) {
+  return (
+    <FormControl component="fieldset" disabled={!toggled}>
+      <FilterButton onClick={(e) => onToggled(e.target.checked)}>
+        {title}
+      </FilterButton>
+      <RadioGroup
+        aria-label={title}
+        name={title}
+        defaultValue={options[selected]?.value}
+      >
+        {options.map((item, index) => (
+          <FormControlLabel
+            key={index}
+            value={item.value}
+            control={<Radio />}
+            label={item.label}
+          />
+        ))}
+      </RadioGroup>
+    </FormControl>
+  )
+}
+
+FilterGroup.propTypes = {
+  toggled: PropTypes.bool,
+  onToggled: PropTypes.func,
+  title: PropTypes.string.isRequired,
+  options: PropTypes.arrayOf({
+    value: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired
+  }),
+  selected: PropTypes.number
+}
+
+FilterGroup.defaultProps = {
+  toggled: false,
+  onToggled: () => {},
+  options: [],
+  selected: 9999
 }
