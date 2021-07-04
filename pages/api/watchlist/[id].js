564a7d5262bdb5c8c2b3845e81502a70a0ca1c00
azer <azerprojects@gmail.com>
2021-07-04 15:10:41 +0700

    fix(api): remove Math.random

--- pages/api/watchlist/[id].js
+++ pages/api/watchlist/[id].js
@@ -1,6 +1,4 @@
 import courses from '@/mocks/data/courses.json'
 export default async function handler(req, res) {
-  return res
-    .status(200)
-    .json({ results: courses.slice(0, Math.random() * 20 + 10) })
+  return res.status(200).json({ results: courses.slice(0, 10) })
 }
